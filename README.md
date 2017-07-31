a4b2f32b1aa9abd00bdfa9475630f775d8c18390
CraneJen <hwqzqh@gmail.com>
2017-07-31 13:41:27 +0800

    Add user register

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FlaskApp
 A simple project built with flask
+
+
+DATABASE Postgresql
+TABLE users:
+CREATE TABLE users (
+    id serial,
+    name varchar(100),
+    username varchar(100),
+    email varchar(150) not null,
+    password varchar(100) not null,
+    PRIMARY KEY(id, username)
+    )
--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
+import psycopg2
+# from psycopg2.extensions import parse_dsn
+from wtforms import Form, TextField, StringField, PasswordField, validators
+from passlib.hash import sha256_crypt
+import os
+
 
 app = Flask(__name__)
+# config  Postgresql
+conn = psycopg2.connect(os.environ['PSQL_DSN'])
+# secret key
+app.secret_key = os.environ['SECRET_KEY']
+# debug
 app.debug = True
 
 
@@ -17,6 +28,45 @@ def about():
 @app.route('/articles')
 def articles():
     return render_template('articles.html')
+
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=1, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password do not match!')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = conn.cursor()
+
+        try:
+            cur.execute("INSERT INTO users(name,username,email,password) VALUES(%s, %s, %s, %s)", (name, username, email, password))
+            conn.commit()
+            # close connect
+            cur.close()
+            flash('You are now register adn can log in!!', 'success')
+            return redirect(url_for('index'))
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            # cur.close()
+            flash('The username is be registerd!', 'warning')
+
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
